003522b7d4d367663659f09ec973f51e64ffaf82
Kirill Tronin <troninkirill@gmail.com>
2021-03-30 23:22:04 +0400

    remove default createreactapp serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Portfolio from './components/Portfolio';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
@@ -28,8 +27,3 @@ store.subscribe(() => {
 })
 
 store.subscribe(render);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
